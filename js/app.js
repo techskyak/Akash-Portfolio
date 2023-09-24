c927a552a1bc9239a9d5ab0a7d5ae0f7ff1ec7a7
Akash <akasheducation100@gmail.com>
2023-09-24 22:39:20 +0530

    adding custom cursor

--- js/app.js
+++ js/app.js
@@ -111,6 +111,23 @@ window.addEventListener("load", function () {
       }, delayMillis);
    });
 
-
+   const cursoreDot = document.querySelector("[data-cursor-dot]");
+   const cursorOutline = document.querySelector("[data-cursor-outline]");
+   
+   window.addEventListener("mousemove", function (e) {
+       const posX = e.clientX;
+       const posY = e.clientY;
+   
+       cursoreDot.style.left = `${posX}px`;
+       cursoreDot.style.top = `${posY}px`;
+   
+       // cursorOutline.style.left = `${posX}px`;
+       // cursorOutline.style.top = `${posY}px`;
+   
+       cursorOutline.animate({
+           left:`${posX}px`,
+           top:`${posY}px`
+       }, { duration:500, fill: "forwards"});
+   });
 
 
